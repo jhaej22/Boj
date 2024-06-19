add2a32888b14b4fe1f7ef49ac5059a81383c763
haejoon <jhaej0722@gmail.com>
2024-06-19 20:37:57 +0900

    EOF에 대한 개념, hasNextInt()메서드 개념 확립

--- src/algorithm/Ex10951.java
+++ src/algorithm/Ex10951.java
@@ -6,15 +6,19 @@ public class Ex10951 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        while (true) {
+        while (sc.hasNextInt()) {
             int a = sc.nextInt();
             int b = sc.nextInt();
-
-            if (a <= 0 || b >= 10) {
-                break;
-            }
-
             System.out.println(a + b);
         }
     }
 }
+
+// java.util.Scanner.hasNextInt() 메서드
+// public boolean hasNextInt()
+// 반환값: Scanner 객체에 입력값이 int값일 때만 true를 반환
+
+// 동작 과정
+// 입력값이 들어올 때까지 while문 내부가 실행되지 않다가
+// 입력값이 들어온 후 hasNextInt()메서드가 참 거짓 값을 반환시킨다.
+// 그후 while문 내부가 실행됨
