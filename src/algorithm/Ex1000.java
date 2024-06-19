a38e9c4cd2cba37070ef5fc28783e97987839eec
haejoon <jhaej0722@gmail.com>
2024-06-19 17:43:52 +0900

    commit and push

---
+++ src/algorithm/Ex1000.java
@@ -0,0 +1,13 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Ex1000 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(a + b);
+    }
+}
