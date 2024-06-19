845ab7ab9112af94c9e41d9de8a6949e9cb8ffdf
haejoon <jhaej0722@gmail.com>
2024-06-19 20:13:32 +0900

    commit and push

---
+++ src/algorithm/Ex10952.java
@@ -0,0 +1,19 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Ex10952 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if (a == 0 && b == 0) {
+                break;
+            }
+            System.out.println(a + b);
+        }
+
+    }
+}
