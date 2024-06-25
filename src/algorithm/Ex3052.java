a59976f63c8cf86f6da5b7555f0386c1cf8230c6
haejoon <jhaej0722@gmail.com>
2024-06-25 18:01:27 +0900

    commit and push

--- src/algorithm/Ex3052.java
+++ src/algorithm/Ex3052.java
@@ -1,33 +1,33 @@
 package algorithm;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Ex3052 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] arr = new int[10];
-        boolean bl;
-        int count = 0;
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int[] arr = new int[10];
+            boolean bl;
+            int count = 0;
 
-        for (int i = 0; i < 10; i++) {
-            arr[i] = Integer.parseInt(br.readLine()) % 42;
-        }
+            for (int i = 0; i < 10; i++) {
+                arr[i] = Integer.parseInt(br.readLine()) % 42;
+            }
 
-        for (int i = 0; i < 10; i++) {
-            bl = false;
-            for (int j = i + 1; j < 10; j++) {
-                if (arr[i] == arr[j]) {
-                    bl = true;
-                    break;
+            for (int i = 0; i < 10; i++) {
+                bl = false;
+                for (int j = i + 1; j < 10; j++) {
+                    if (arr[i] == arr[j]) {
+                        bl = true;
+                        break;
+                    }
+                }
+                if (bl == false) {
+                    count++;
                 }
             }
-            if (bl == false) {
-                count++;
-            }
+            System.out.println(count);
         }
-        System.out.println(count);
     }
 }
-
-
-
