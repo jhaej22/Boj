c33e48b66844a113dc12c16ce79e5e7a85d14d22
haejoon <jhaej0722@gmail.com>
2024-06-26 17:17:27 +0900

    commit and push

---
+++ src/algorithm/Ex2743.java
@@ -0,0 +1,12 @@
+package algorithm;
+
+import java.io.*;
+
+public class Ex2743 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = br.readLine();
+
+        System.out.println(word.length());
+    }
+}
