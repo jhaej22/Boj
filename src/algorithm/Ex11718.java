d15609c8350ecc1dcc0b11e69b768cec32b1cbd8
haejoon <jhaej0722@gmail.com>
2024-06-28 18:25:43 +0900

    Commit and Push

--- src/algorithm/Ex11718.java
+++ src/algorithm/Ex11718.java
@@ -2,9 +2,18 @@ package algorithm;
 
 import java.io.*;
 
+/**
+ * 입력 받을 때 종료 조건을 어떻게 설정할까에 대한 것이 문제의 포인트
+ */
+
 public class Ex11718 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        String s;
+
+        while ((s = br.readLine()) != null) {
+            System.out.println(s);
+        }
     }
 }
