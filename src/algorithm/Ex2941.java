eed079d1ff5f49cbf9a1626ae80edbe1ec22969d
haejoon <jhaej0722@gmail.com>
2024-07-06 14:41:49 +0900

    ver.2

--- src/algorithm/Ex2941.java
+++ src/algorithm/Ex2941.java
@@ -7,26 +7,31 @@ public class Ex2941 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
         int count = s.length();
+        int len = s.length();
 
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == 'c') {
+        for (int i = 0; i < len; i++) {
+            if (s.charAt(i) == 'c' && i < len - 1) {
                 if (s.charAt(i + 1) == '=' || s.charAt(i + 1) == '-') {
                     count--;
                     i++;
                 }
-            } else if (s.charAt(i) == 'd') {
-                if (s.charAt(i + 1) == 'z' && s.charAt(i + 2) == '=') {
-                    count -= 2;
-                    i += 2;
-                } else if (s.charAt(i + 1) == '-') {
+            } else if (s.charAt(i) == 'd' && i < len - 1) {
+                if (s.charAt(i + 1) == '-') {
+                    count--;
+                    i++;
+                } else if (s.charAt(i + 1) == 'z' && i < len - 2) {
+                    if (s.charAt(i + 2) == '=') {
+                        count -= 2;
+                        i += 2;
+                    }
+                }
+
+            } else if ((s.charAt(i) == 'l' || s.charAt(i) == 'n') && i < len - 1) {
+                if (s.charAt(i + 1) == 'j') {
                     count--;
                     i++;
                 }
-            } else if (s.charAt(i) == 'j') {
-                if (s.charAt(i - 1) == 'l' || s.charAt(i - 1) == 'n') {
-                    count--;
-                }
-            } else if (s.charAt(i) == 's' || s.charAt(i) == 'z') {
+            } else if ((s.charAt(i) == 's' || s.charAt(i) == 'z') && i < len - 1) {
                 if (s.charAt(i + 1) == '=') {
                     count--;
                     i++;
