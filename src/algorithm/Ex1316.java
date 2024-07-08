12d8d12d57e407135c68b0a1f4e1148835c4bfdd
haejoon <jhaej0722@gmail.com>
2024-07-08 11:14:35 +0900

    commit and push

--- src/algorithm/Ex1316.java
+++ src/algorithm/Ex1316.java
@@ -5,7 +5,6 @@ import java.io.*;
 public class Ex1316 {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
     public static void main(String[] args) throws IOException{
         int n = Integer.parseInt(br.readLine());
         int count = 0;
@@ -15,27 +14,29 @@ public class Ex1316 {
                 count++;
             }
         }
+
         System.out.println(count);
     }
 
-    public static boolean check() throws IOException {
-        boolean[] check = new boolean[26];
-        int prev = 0;
+    public static boolean check() throws IOException{
+        boolean[] arr = new boolean[26];
         String str = br.readLine();
+        int prev = 0;
 
         for (int i = 0; i < str.length(); i++) {
             int now = str.charAt(i);
 
-            if (prev != now) { // 앞의 문자와 서로 다를 때
-                if (!check[now - 'a']) {
-                    check[now - 'a'] = true; // 처음 나온 문자
+            if (prev != now) {
+                if (!arr[now - 'a']) {
+                    arr[now - 'a'] = true;
                     prev = now;
                 } else {
-                    return false; // 이미 나왔던 문자이지만 불연속 일 때
+                    return false;
                 }
             }
         }
 
-        return true; // 연속된 문자가 나온 경우
+        return true;
     }
+
 }
