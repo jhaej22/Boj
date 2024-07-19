387d79f0b93fb04463b8163545ab717da92bec51
haejoon <jhaej0722@gmail.com>
2024-07-19 23:24:23 +0900

    commit and push

---
+++ src/algorithm/Ex24262.java
@@ -0,0 +1,16 @@
+package algorithm;
+
+/**
+ * 알고리즘의 수행시간 1
+ */
+public class Ex24262 {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(0);
+        // n(인풋)에 어떠한 값이 들어오더라도
+        // i = n / 2 이고
+        // 배열의 i번째(인덱스)의 값을 반환
+        // 즉, 무조건 코드는 단 한번만 실행되고
+        // 빅오 표기법으로 O(1)이 됨
+    }
+}
