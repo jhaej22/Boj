74861ef50320b2c3c4cb1780accb961eecd5fe5b
haejoon <jhaej0722@gmail.com>
2024-07-20 20:33:52 +0900

    commit and push

---
+++ src/algorithm/Ex24263.java
@@ -0,0 +1,22 @@
+package algorithm;
+
+import java.io.*;
+
+/**
+ * 알고리즘의 수행시간 2
+ * o(1) + n * o(1) + o(1)
+ * = n * o(1)
+ * = o(n)
+ */
+
+public class Ex24263 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(n);
+        System.out.println(1);
+    }
+}
+
+
